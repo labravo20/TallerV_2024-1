37563a9beada1bbf709519b9c455a640eddc6882
labravo20 <labravo@unal.edu.co>
2024-03-07 11:02:50 -0500

    Update main.c
    Cambios iniciales en archivo main

--- BasicConfig/Src/main.c
+++ BasicConfig/Src/main.c
@@ -17,13 +17,17 @@
  */
 
 #include <stdint.h>
-
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+#include <stdio.h>
 
 int main(void)
 {
+
+	printf("Hello world!!!");
+
     /* Loop forever */
-	for(;;);
+	while(1){
+
+	}
+
+	return 0;
 }
