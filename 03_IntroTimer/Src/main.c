15b260131aa40b7e8667a1529c26710c0a0ca308
labravo20 <labravo@unal.edu.co>
2024-08-05 18:28:54 -0500

    Configuración IntroTimer
    Descripción de configuración inicial función main.c

--- 03_IntroTimer/Src/main.c
+++ 03_IntroTimer/Src/main.c
@@ -4,31 +4,22 @@
  * @author         : laurasofia
  * @brief          : Configuración básica de un proyecto.
  ******************************************************************************
- * @attention
- *
- * Copyright (c) 2024 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
  */
 
 #include <stdint.h>
 #include "stm32f4xx.h"
 #include "stm32_assert.h"
 #include "gpio_driver_hal.h"
+#include "timer_driver_hal.h"
 
-//Definición de pines a utilizar
+//Definimos pin de prueba
 GPIO_Handler_t userLed    = {0}; //PinA5
+
+Timer_Handler_t blinkTimer = {0};
 
 /*  Main function  */
 int main(void)
 {
-	//VERIFIFICACIÓN DE BasicConfig del UpdateGPIO
-
 	/* Configuramos el pin */
 	userLed.pGPIOx                         = GPIOA;
 	userLed.pinConfig.GPIO_PinNumber       = PIN_5;
@@ -43,16 +34,39 @@ int main(void)
 	//Ejecutamos la configuración realizada
 	gpio_WritePin(&userLed, SET);
 
+	blinkTimer.pTIMx                             = TIM2;
+	blinkTimer.TIMx_Config.TIMx_Prescaler        = 16000;  //Genera incrementos de 1 ms
+	blinkTimer.TIMx_Config.TIMx_Period           = 250;     //De la mano con el prescaler...
+	blinkTimer.TIMx_Config.TIMx_mode             = TIMER_UP_COUNTER;
+	blinkTimer.TIMx_Config.TIMx_InterruptEnable  = TIMER_INT_ENABLE;
+
+	/* Configuramos el Timer */
+	timer_Config(&blinkTimer);
+
+	//Encendemos el Timer
+	timer_SetState(&blinkTimer, TIMER_ON);
 
     /* Loop forever */
 	while(1){
 
-		gpio_TooglePin(&userLed);
-		for (uint32_t j=0; j < 200000; j++){
-			__NOP();
-		}
 
 	}
 
-	return 0;
+}
+
+/*
+ * Overwrite function
+ * */
+void Timer2_Callback(void){
+	gpio_TooglePin(&userLed);
+}
+
+/*
+ * Esta función sirve para detectar problemas de parámetros
+ * incorrectos al momento de ejecutar un programa.
+ * */
+void assert_failed(uint8_t* file, uint32_t line){
+	while(1){
+		//Problems...
+	}
 }
