84f711665e65e9996228bda3fd40229f2a6f79e8
labravo20 <labravo@unal.edu.co>
2024-08-04 22:06:51 -0500

    Comprobando buen funcionamiento de UpdateGPIO configuración inicial.

--- 02_UpdateGPIO/Src/main.c
+++ 02_UpdateGPIO/Src/main.c
@@ -16,66 +16,41 @@
  ******************************************************************************
  */
 
-#include <stm32f4xx.h>
 #include <stdint.h>
-#include <stdio.h>
-#include "Ejemplo.h"
+#include "stm32f4xx.h"
+#include "stm32_assert.h"
+#include "gpio_driver_hal.h"
 
-//Definicion constantes
+//Definición de pines a utilizar
+GPIO_Handler_t userLed    = {0}; //PinA5
 
-#define RCC_BASE_ADDRESS      0x40023800UL
-#define RCC_AHB1ENR_OFFSET    0x30
-#define RCC_AHB1ENR           (RCC_BASE_ADDRESS + RCC_AHB1ENR_OFFSET)
-
-#define GPIOA_BASE_ADDRESS    0x40020000UL
-#define GPIOA_MODE_REG_OFFSET 0x00
-#define GPIOA_MODE_ODR_OFFSET 0x14
-#define GPIOA_MODE_REG        (GPIOA_BASE_ADDRESS + GPIOA_MODE_REG_OFFSET)
-#define GPIOA_OPD_REG         (GPIOA_BASE_ADDRESS + GPIOA_MODE_ODR_OFFSET)
-
-//Definicion variables
-BasicExample ejemploClase = {0};
-BasicExample datosEjemplo = {0};
-
-//uint32_t *registerAHB1enb;
-unsigned int day;
-
+/*  Main function  */
 int main(void)
 {
+	//VERIFIFICACIÓN DE BasicConfig del UpdateGPIO
 
-	uint32_t *registerAHB1enb = (uint32_t *)RCC_AHB1ENR;
-	//uint32_t *registerAHB1enb = (uint32_t *)0x40023830UL;
+	/* Configuramos el pin */
+	userLed.pGPIOx                         = GPIOA;
+	userLed.pinConfig.GPIO_PinNumber       = PIN_5;
+	userLed.pinConfig.GPIO_PinMode         = GPIO_MODE_OUT;
+	userLed.pinConfig.GPIO_PinOutputType   = GPIO_OTYPE_PUSHPULL;
+	userLed.pinConfig.GPIO_PinOutputSpeed  = GPIO_OSPEED_MEDIUM;
+	userLed.pinConfig.GPIO_PinPuPdControl  = GPIO_PUPDR_NOTHING;
 
-	//ESTAMOS ENCENDIENDO LA SEÑAL DE RELOJ PARA PUERTOS ESPECÍFICOS
-	*registerAHB1enb |= (1 << 2); //Activando la señal de reloj para el puerto GPIOc
-	*registerAHB1enb |= (1 << 0); //Activando la señal de reloj para el puerto GPIOA
+	//Cargamos la configuración en los registros que gobiernan el puerto
+	gpio_Config(&userLed);
 
-	uint32_t *registerGPIOA_MODE = (uint32_t *)GPIOA_MODE_REG;
-	*registerGPIOA_MODE |= (1 << 10);
+	//Ejecutamos la configuración realizada
+	gpio_WritePin(&userLed, SET);
 
-	uint32_t *registerGPIOA_ODR = (uint32_t *)GPIOA_OPD_REG;
-	*registerGPIOA_ODR |= (1 << 5); //LED2 (green) set
-
-	*registerGPIOA_ODR &= ~(1 << 5); //LED2 (green) off
-	*registerGPIOA_ODR |= (0 << 5); //LED2 (green) off
-
-	ejemploClase.ID = 'w';
-	ejemploClase.counterUp = 100;
-	ejemploClase.dummy = 25;
-
-	datosEjemplo.ID = 'f';
-	datosEjemplo.dummy = 28;
-	datosEjemplo.timestamp = 0x1234;
-	//Elemento tipo float: cuando estamos trabajando con elementos de punto flotante se anexa la "f"
-	datosEjemplo.promedio = 32.45f;
-
-	for(datosEjemplo.counterUp=0;datosEjemplo.counterUp < ejemploClase.dummy;datosEjemplo.counterUp++){
-		datosEjemplo.promedio += 0.25f;
-		ejemploClase.counterUp -=2;
-	}
 
     /* Loop forever */
 	while(1){
+
+		gpio_TooglePin(&userLed);
+		for (uint32_t j=0; j < 200000; j++){
+			__NOP();
+		}
 
 	}
 
