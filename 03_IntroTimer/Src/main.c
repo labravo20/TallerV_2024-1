831104b2a3992671ec5de0e6841fbf319c8190fc
labravo20 <labravo@unal.edu.co>
2024-08-05 13:51:36 -0500

    Creación proyecto IntroTimer
    Este proyecto es una copia del UpdateGPIO y de todas sus configuraciones respectivas.

---
+++ 03_IntroTimer/Src/main.c
@@ -0,0 +1,58 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : laurasofia
+ * @brief          : Configuración básica de un proyecto.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "stm32_assert.h"
+#include "gpio_driver_hal.h"
+
+//Definición de pines a utilizar
+GPIO_Handler_t userLed    = {0}; //PinA5
+
+/*  Main function  */
+int main(void)
+{
+	//VERIFIFICACIÓN DE BasicConfig del UpdateGPIO
+
+	/* Configuramos el pin */
+	userLed.pGPIOx                         = GPIOA;
+	userLed.pinConfig.GPIO_PinNumber       = PIN_5;
+	userLed.pinConfig.GPIO_PinMode         = GPIO_MODE_OUT;
+	userLed.pinConfig.GPIO_PinOutputType   = GPIO_OTYPE_PUSHPULL;
+	userLed.pinConfig.GPIO_PinOutputSpeed  = GPIO_OSPEED_MEDIUM;
+	userLed.pinConfig.GPIO_PinPuPdControl  = GPIO_PUPDR_NOTHING;
+
+	//Cargamos la configuración en los registros que gobiernan el puerto
+	gpio_Config(&userLed);
+
+	//Ejecutamos la configuración realizada
+	gpio_WritePin(&userLed, SET);
+
+
+    /* Loop forever */
+	while(1){
+
+		gpio_TooglePin(&userLed);
+		for (uint32_t j=0; j < 200000; j++){
+			__NOP();
+		}
+
+	}
+
+	return 0;
+}
