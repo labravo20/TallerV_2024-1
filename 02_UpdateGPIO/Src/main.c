4a930fcbb059081909e505349fdf61570c25a006
labravo20 <labravo@unal.edu.co>
2024-08-08 10:43:32 -0500

    Verificación update GPIO
    Configuración de blinking con pin H1 para verificación de correcto funcionamiento del código.

--- 02_UpdateGPIO/Src/main.c
+++ 02_UpdateGPIO/Src/main.c
@@ -22,14 +22,15 @@
 #include "gpio_driver_hal.h"
 
 //Definición de pines a utilizar
-GPIO_Handler_t userLed    = {0}; //PinA5
+GPIO_Handler_t userLed      = {0}; //PinA5
+GPIO_Handler_t userLed01    = {0}; //PinH1
 
 /*  Main function  */
 int main(void)
 {
 	//VERIFIFICACIÓN DE BasicConfig del UpdateGPIO
 
-	/* Configuramos el pin */
+	/* Configuramos el pin A5 */
 	userLed.pGPIOx                         = GPIOA;
 	userLed.pinConfig.GPIO_PinNumber       = PIN_5;
 	userLed.pinConfig.GPIO_PinMode         = GPIO_MODE_OUT;
@@ -40,14 +41,34 @@ int main(void)
 	//Cargamos la configuración en los registros que gobiernan el puerto
 	gpio_Config(&userLed);
 
-	//Ejecutamos la configuración realizada
-	gpio_WritePin(&userLed, SET);
+	//Ejecutamos la configuración realizada del pin A5
+	//gpio_WritePin(&userLed, SET);
 
+	/* Configuramos el pin H1*/
+		userLed01.pGPIOx                         = GPIOH;
+		userLed01.pinConfig.GPIO_PinNumber       = PIN_1;
+		userLed01.pinConfig.GPIO_PinMode         = GPIO_MODE_OUT;
+		userLed01.pinConfig.GPIO_PinOutputType   = GPIO_OTYPE_PUSHPULL;
+		userLed01.pinConfig.GPIO_PinOutputSpeed  = GPIO_OSPEED_MEDIUM;
+		userLed01.pinConfig.GPIO_PinPuPdControl  = GPIO_PUPDR_NOTHING;
+
+		//Cargamos la configuración en los registros que gobiernan el puerto
+		gpio_Config(&userLed01);
+
+		//Ejecutamos la configuración realizada del pin H1
+		//gpio_WritePin(&userLed01, SET);
 
     /* Loop forever */
 	while(1){
 
-		gpio_TooglePin(&userLed);
+		//Llamamos función TooglePin para pin A5
+		//gpio_TooglePin(&userLed);
+		//for (uint32_t j=0; j < 200000; j++){
+			//__NOP();
+		//}
+
+		//Llamamos función TooglePin para pin H1
+		gpio_TooglePin(&userLed01);
 		for (uint32_t j=0; j < 200000; j++){
 			__NOP();
 		}
