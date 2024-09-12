db2f41f63247fb6ec4f0458d271ed6ee3db011a6
labravo20 <labravo@unal.edu.co>
2024-09-12 11:27:35 -0500

    Peripheral drivers
    Creación documento .h para driver I2C

---
+++ PeripheralsDrivers/Inc/i2c_driver_hal.h
@@ -0,0 +1,13 @@
+/*
+ * i2c_driver_hal.h
+ *
+ *  Created on: 12/09/2024
+ *      Author: laurasofia
+ */
+
+#ifndef I2C_DRIVER_HAL_H_
+#define I2C_DRIVER_HAL_H_
+
+
+
+#endif /* I2C_DRIVER_HAL_H_ */
